15f9b7c074b746f102d47e17105ff2ee83e39443
Rxses <35810818+rxses-x@users.noreply.github.com>
2024-10-31 20:23:05 +0100

    feat(Banner): update padding and style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,9 @@
 function Banner({title, image}) {
     return (
-        <div>
-            <main className="banner-content">
-                <img src={image} alt="" className="banner-content__image"></img>
-                <h1 className="banner-content__title">{title}</h1>
-            </main>
-        </div>
+        <main className="banner-content">
+            <img src={image} alt="" className="banner-content__image"></img>
+            <h1 className="banner-content__title">{title}</h1>
+        </main>
     )
 }
 
